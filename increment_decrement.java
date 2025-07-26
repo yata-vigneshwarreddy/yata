918245730f2dd51e240554bb9e5844d1531f1716
yata-vigneshwarreddy <2420030203@klh.edu.in>
2025-07-26 11:17:55 +0530

    Add files via upload

---
+++ increment_decrement.java
@@ -0,0 +1,25 @@
+package oops_practicals;
+import java.util.Scanner;
+
+public class increment_decrement {
+	
+		  public static void main(String[] args) {
+		    
+
+		    int a = 12, b = 12;
+		    int result1, result2;
+
+
+		    System.out.println("Value of a: " + a);
+
+		    		    result1 = ++a;
+		    System.out.println("After increment: " + result1);
+
+		    System.out.println("Value of b: " + b);
+
+		    result2 = --b;
+		    System.out.println("After decrement: " + result2);
+		  }
+		}
+
+
